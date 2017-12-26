d2fb7cd1c1bf1976e0ecb622ca0cf4e2ff66ee22
taka <521taka.m@gmail.com>
2017-12-26 22:18:50 +0900

    ステップ3.4:アトミックカウンターを増分する、を作成

---
+++ tutorial-node-dynamodb/app/MoviesItemOps04.js
@@ -0,0 +1,40 @@
+'use strict';
+
+let AWS = require('aws-sdk');
+
+AWS.config.update({
+    region: "us-east-1",
+    endpoint: "http://localhost:4569",
+    accessKeyId: "dummy",
+    secretAccessKey: "dummy"
+});
+
+let docClient = new AWS.DynamoDB.DocumentClient();
+
+let table = "Movies";
+
+let year = 2015;
+let title = "The Big New Movie";
+
+let params = {
+    TableName: table,
+    Key: {
+        "year": year,
+        "title": title
+    },
+    UpdateExpression: "set info.rating = info.rating + :val",
+    ExpressionAttributeValues: {
+        ":val": 1
+    },
+    ReturnValues: "UPDATED_NEW"
+};
+
+console.log("Updating the item...");
+
+docClient.update(params, (err, data) => {
+    if (err) {
+        console.error("Unable to update item. Error JSON:", JSON.stringify(err, null, 2));
+    } else {
+        console.log("UpdateItem succeeded:", JSON.stringify(data, null, 2));
+    }
+});
